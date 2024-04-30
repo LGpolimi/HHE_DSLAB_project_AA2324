0f6a05e11ad0e0924ace643630272590a2058669
eleonora11sala <eleonora1.sala@mail.polimi.it>
2024-04-30 16:16:23 +0200

    Creation of green areas complete dataset

--- create_green_dataset.py
+++ create_green_dataset.py
@@ -2,23 +2,49 @@ import pandas as pd
 import numpy as np
 
 ## Import datasets
-tree_density = pd.read_csv('Datasources/histograms_landcover.csv') #Import dataset about tree density
-#grass_cover = pd.read_csv() #Import dataset about grass cover
-#small_woods = pd.read_csv() #Import dataset about small woods
-#land_cover = pd.read_csv() #Import dataset about land cover
+tree_density = pd.read_csv('Datasources/tree_density.csv') #Import dataset about tree density
+tree_density_binary = pd.read_csv('Datasources/tree_density_binary.csv') #Import dataset about tree density binary
+grass_cover = pd.read_csv('Datasources/grass_stats.csv') #Import dataset about grass cover
+small_woods = pd.read_csv('Datasources/small_woods.csv') #Import dataset about small woods
+land_cover = pd.read_csv('Datasources/histograms_landcover.csv') #Import dataset about land cover
 
 
+# Add grass cover
+print(grass_cover.head(5))
+
+final=grass_cover[["COMUNE","Shape_Area","_sum"]]
+final.rename(columns={'_sum':'Grass_cover'})
+print(final.tail(10))
+
+for i in range(1,10):
+    final.loc[[132+i],'COMUNE'] = 'Municipio'+str(i)
+    final.loc[[132 + i], 'Shape_Area'] = grass_cover.loc[[132 + i],'AREA']
+
+# Add small woods
+print(small_woods.head(5))
+
+final['Small_woods_cover']=small_woods["_sum"].copy()
+print(final.tail(10))
+
+# Add tree density
+print(tree_density_binary.head(5))
 print(tree_density.head(5))
 
+final['Trees_cover']=tree_density_binary["_sum"].copy()
+final['Trees_count']=tree_density["_sum"].copy()
+final['Trees_mean_density']=tree_density["_mean"].copy()
 
-final=tree_density["COMUNE"].to_frame()
+print(final.tail(10))
 
-# drop rows by index
-final.drop(range(133,142), inplace = True)
-municipi=[]
-for i in range(1,10):
-    municipi = np.append([municipi],['Municipio'+str(i)])
-final = pd.concat([final,pd.DataFrame(columns=['COMUNE'],data=municipi)], ignore_index=True)
+# Add land cover
+print(land_cover.head(5))
+
+new_col=['Non_class_surf','Imper_surf','Perm_surf','Non_cons_surf','Hardwood','Coniferous','Shrubs','Periodic_herb','Permanent_herb','Permanent_water','Wetlands']
+final[new_col]=land_cover.iloc[:,-11:].copy()
+
+print(final.tail(10))
+
+
 
 
 
